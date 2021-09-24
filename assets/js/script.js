7a250551024448f158309c70bda0457461ddba78
andi <andrea.strickland1990@gmail.com>
2021-09-23 19:43:50 -0500

    theoretically finished by adding loops involving modulus but still need to debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var cityInput = $('#cityInput');
 var searchBtnEl = $('#searchBtn');
 var cityAppenderEl = $('#cityAppender')
 var cityFormEl = $('#city-form');
+var currentCity= $('#currentCity');
+var today = moment().format('MM/DD/YYYY');
 
 //this section pulls the stored city history and sets it when entered
 function init() {
@@ -105,6 +107,25 @@ function searchApiCurrent(cityName) {
         dataType: 'json',
     	success: function(data){ 
     	  console.log(data);
+
+          var iconcode = data.weather[0].icon;
+          var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+         var icon = $('<img>').attr('src', iconurl);
+            currentCity.html('<strong>'+ cityName +'</strong> &nbsp;' 
+            + today + '&nbsp;' );
+            currentCity.append(icon);
+
+        $('#cTemp').html('Temp: &nbsp;' + data.main.temp + '°F');
+        $('#cWind').html('Wind: &nbsp;' + data.wind.speed+ 'MPH');
+        $('#cHumidity').html('Humidity: &nbsp;' + data.main.humidity + '%');
+        $('#cUV').html('UV Index: &nbsp;');    
+
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        console.log(lat);
+        console.log(lon);
+        setUV(lat, lon);
+
     	},
     	error: function(){
     		alert("There was an error.");
@@ -113,6 +134,47 @@ function searchApiCurrent(cityName) {
 
 }
 
+
+function setUV(lat, lon){
+   
+    var queryUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat + "&lon=" + lon + "&appid=478e401a93735bdfa0c38415c842b721";
+
+    $.ajax({
+        url: queryUrl,
+        dataType: 'json',
+        success: function(data){ 
+     console.log(data);
+
+     var uvi = data.current.uvi;
+    var uvBadge =  $('<button>');
+    uvBadge.addClass('btn');
+    uvBadge.attr('type', 'button');
+    
+    uvBadge.text(uvi);
+
+
+     if (uvi<=2){
+        uvBadge.addClass('btn-success');
+
+     }else if (uvi <=5){
+        uvBadge.addClass('btn-warning');
+
+     }else{
+        uvBadge.addClass('btn-danger');
+
+     };
+
+     $('#cUV').append(uvBadge);
+
+        },
+        error: function(){
+            alert("There was an error.");
+        }
+    });
+
+}
 
 
     function searchApiForecast(cityName) {    
@@ -133,6 +195,54 @@ function searchApiCurrent(cityName) {
             dataType: 'json',
             success: function(data){ 
          console.log(data);
+        var forecast= $('#forecast');
+     
+
+      for(i=0; i<41; i++){
+        while(i%8==0) {
+        var iconcode =  data.list[i].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+       var icon = $('<img>').attr('src', iconurl);
+
+       var reformatDate = moment(data.list[i].dt_text, "YYYY-MM-DD HH:mm:ss").format("MM-DD-YYYY");
+     var container= $('<div>').addClass('col-sm-2');
+        forecast.append(container);
+        var card = $('<div>').addClass('card')
+            .addClass('bg-light').addClass('mb-3')
+            .attr('style', 'max-width:18rem;');
+        container.append(card);
+       
+        var header = $('<h5>').addClass('card-header')
+        .text(reformatDate);
+        card.append(header);
+
+        var body =$('</div>').addClass('card-body');
+        header.append(body);
+
+        var title =$('<div>').addClass('card-title');
+        body.append(title);
+        title.append(icon);
+
+        var cardText = $('<p>').addClass('card-text');
+        body.append(cardText);
+
+        var uList= $('<ul>').addClass('list-group').addClass('list-group-flush');
+        cardText.append(uList);
+
+        var temp= $('<li>').addClass('list-group-item').addClass('mt-1')
+        .text('Temp: &nbsp;' + data.list[i].main.temp + '°F')
+        uList.append(temp);
+
+        var wind= $('<li>').addClass('list-group-item').addClass('mt-1')
+        .text('Wind: &nbsp;' + data.list[i].wind.speed + 'MPH')
+        uList.append(wind);
+
+        var humidity= $('<li>').addClass('list-group-item').addClass('mt-1')
+        .text('Humidity: &nbsp;' + data.list[i].main.humidity + '%')
+        uList.append(humidity);
+
+        }
+      }
 
             },
             error: function(){
